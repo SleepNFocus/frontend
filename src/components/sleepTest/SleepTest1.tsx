0b5e2e6df03023c6972b3637170cd6be28a39e34
neuliii <phn01113@gmail.com>
2025-05-30 03:39:10 +0900

    design: 수면 테스트 랜딩페이지 및 설명페이지 UI 구현

---
+++ src/components/sleepTest/SleepTest1.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+
+export  default function SleepTest1 () {
+
+    return(
+        <View>
+            <Text> 첫 번째 수면 테스트 화면입니다. </Text>
+        </View>
+    )
+}
