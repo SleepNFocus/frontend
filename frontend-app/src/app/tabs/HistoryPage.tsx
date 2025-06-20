1a39dd3f74a1aa027d5d0dbde0dfb4f65982b3c1
neuliii <phn01113@gmail.com>
2025-06-20 20:12:38 +0900

    design: 수면 기록 조회 UI 디자인

--- frontend-app/src/app/tabs/HistoryPage.tsx
+++ frontend-app/src/app/tabs/HistoryPage.tsx
@@ -4,11 +4,14 @@ import { useSleepRecordList } from '@/services/recordListApi';
 import { Text } from '@/components/common/Text';
 import { DayRecord, MonthRecord, WeekRecord } from '@/types/history';
 import { colors } from '@/constants/colors';
-import { spacing } from '@/utils/responsive';
+import { fontSize, spacing } from '@/utils/responsive';
 import { black } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '@/App';
+import { Button } from '@/components/common/Button';
+import { Card } from '@/components/common/Card';
+import { Layout } from '@/components/common/Layout';
 
 const periodOptions = ['day', 'week', 'month'] as const;
 type Period = (typeof periodOptions)[number];
@@ -19,94 +22,109 @@ export const HistoryPage: React.FC = () => {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {/* 필터 버튼 */}
-      <View style={styles.filterContainer}>
-        {periodOptions.map(p => (
-          <TouchableOpacity
-            key={p}
-            onPress={() => setPeriod(p)}
-            style={[
-              styles.filterButton,
-              period === p && styles.activeFilterButton,
-            ]}
-          >
-            <Text>{p === 'day' ? '일별' : p === 'week' ? '주별' : '월별'}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      {/* 상태 표시 */}
-      {isLoading && <Text style={styles.statusText}>로딩 중...</Text>}
-      {error && (
-        <Text style={[styles.statusText]}>
-          에러 발생:{' '}
-          {error instanceof Error ? error.message : '알 수 없는 오류'}
-        </Text>
-      )}
-
-      {/* 일별 */}
-      {period === 'day' &&
-        (data?.results as DayRecord[])?.map((item, index) => (
-          <TouchableOpacity
-            key={index}
-            style={styles.card}
-            onPress={() =>
-              navigation.navigate('SleepDetail', { date: item.date })
-            }
-          >
-            <Text>날짜: {item.date}</Text>
-            <Text>수면시간: {item.sleep_hour}시간</Text>
-            <Text>수면점수: {item.sleep_score}점</Text>
-            <Text>인지점수: {item.cognitive_score}점</Text>
-          </TouchableOpacity>
-        ))}
-
-      {/* 주별 */}
-      {period === 'week' &&
-        (data?.results as WeekRecord[])?.map((item, index) => (
-          <View key={index} style={styles.card}>
-            <Text>시작일: {item.start_date}</Text>
-            <Text>종료일: {item.end_date}</Text>
-            <Text>총 수면시간: {item.total_sleep_hours}시간</Text>
-            <Text>평균 수면점수: {item.average_sleep_score}</Text>
-            <Text>평균 인지점수: {item.average_cognitive_score}</Text>
+    <Layout>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.secContainer}>
+          <Text variant="headlineMedium" style={styles.text}>
+            수면 기록 조회
+          </Text>
+          {/* 필터 버튼 */}
+          <View style={styles.filterContainer}>
+            {periodOptions.map(p => (
+              <Button
+                key={p}
+                onPress={() => setPeriod(p)}
+                title={p === 'day' ? '일별' : p === 'week' ? '주별' : '월별'}
+                variant={period === p ? 'primary' : 'outline'}
+                style={styles.filterButton}
+                textStyle={period === p ? styles.activeFilterText : undefined}
+              />
+            ))}
           </View>
-        ))}
+        </View>
 
-      {/* 월별 */}
-      {period === 'month' &&
-        (data?.results as MonthRecord[])?.map((item, index) => (
-          <View key={index} style={styles.card}>
-            <Text>월: {item.month}</Text>
-            <Text>총 수면시간: {item.total_sleep_hours}시간</Text>
-            <Text>평균 수면점수: {item.average_sleep_score}</Text>
-            <Text>평균 인지점수: {item.average_cognitive_score}</Text>
-          </View>
-        ))}
-    </ScrollView>
+        {/* 상태 표시 */}
+        {isLoading && <Text style={styles.statusText}>로딩 중...</Text>}
+        {error && (
+          <Text style={[styles.statusText]}>
+            에러 발생:{' '}
+            {error instanceof Error ? error.message : '알 수 없는 오류'}
+          </Text>
+        )}
+
+        {/* 일별 */}
+        {period === 'day' &&
+          (data?.results as DayRecord[])?.map((item, index) => (
+            <Card
+              key={index}
+              style={styles.card}
+              onPress={() =>
+                navigation.navigate('SleepDetail', { date: item.date })
+              }
+            >
+              <Text style={styles.text} variant="titleLarge">
+                {item.date}
+              </Text>
+              <Text>수면시간 {item.sleep_hour}시간 </Text>
+              <Text>수면점수 {item.sleep_score}점</Text>
+              <Text>인지점수 {item.cognitive_score}점</Text>
+            </Card>
+          ))}
+
+        {/* 주별 */}
+        {period === 'week' &&
+          (data?.results as WeekRecord[])?.map((item, index) => (
+            <Card key={index} style={styles.card}>
+              <Text style={styles.text}>시작일: {item.start_date}</Text>
+              <Text>종료일: {item.end_date}</Text>
+              <Text>총 수면시간: {item.total_sleep_hours}시간</Text>
+              <Text>평균 수면점수: {item.average_sleep_score}</Text>
+              <Text>평균 인지점수: {item.average_cognitive_score}</Text>
+            </Card>
+          ))}
+
+        {/* 월별 */}
+        {period === 'month' &&
+          (data?.results as MonthRecord[])?.map((item, index) => (
+            <Card key={index} style={styles.card}>
+              <Text>월: {item.month}</Text>
+              <Text>총 수면시간: {item.total_sleep_hours}시간</Text>
+              <Text>평균 수면점수: {item.average_sleep_score}</Text>
+              <Text>평균 인지점수: {item.average_cognitive_score}</Text>
+            </Card>
+          ))}
+      </ScrollView>
+    </Layout>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     padding: spacing.lg,
-    paddingTop: 300,
+  },
+  secContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: 40,
+    marginBottom: 10,
+  },
+  title: {
+    textAlign: 'center',
   },
   filterContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     marginBottom: spacing.lg,
+    paddingHorizontal: 60,
   },
   filterButton: {
     flex: 1,
     marginHorizontal: spacing.xs,
     paddingVertical: spacing.sm,
-    borderRadius: spacing.sm,
-    borderWidth: 1,
-    borderColor: 'black',
-    backgroundColor: 'white',
     alignItems: 'center',
+    minWidth: 70,
+    height: 40,
   },
   activeFilterButton: {
     backgroundColor: 'lightgray',
@@ -121,12 +139,20 @@ const styles = StyleSheet.create({
     color: colors.textColor,
   },
   card: {
-    backgroundColor: colors.lightGray,
-    borderRadius: spacing.md,
-    padding: spacing.md,
-    marginBottom: spacing.md,
-    borderWidth: 1,
-    borderColor: colors.mediumLightGray,
+    gap: 3,
+    paddingHorizontal: 40,
+    padding: 10,
+  },
+  text: {
+    paddingVertical: 4,
+  },
+  roxBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+  },
+  defaultText: {
+    fontWeight: 'bold',
   },
 });
 
