6b63cf3cd5813e6e906f43a096be743de31a9372
workface <with.you70@gmail.com>
2025-07-17 21:25:19 +0900

    Update ci_pre_xcodebuild.sh

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -3,7 +3,7 @@
 echo "🔧 Pre-action: Installing CocoaPods dependencies..."
 
 #iOS 폴더로 이동 (Podfile이 있는 위치)
-cd ios
+cd ../../frontend-app/ios
 
 #최신 CocoaPods 설치 시도 (환경에 따라 이미 설치되어 있으면 무시됨)
 gem install cocoapods --no-document
